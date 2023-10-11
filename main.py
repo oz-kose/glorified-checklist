6bff1d35cf9c885319c0a1e451fc7ca265833e30
oz-kose <123957412+oz-kose@users.noreply.github.com>
2023-10-12 01:19:17 +0900

    save/load functions properly now (probably). still need to fix the checkboxes.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import yaml
 import re
 import qdarktheme
-from PyQt5.QtWidgets import QApplication, QTextEdit, QLabel, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QListWidget, QCheckBox, QLineEdit, QPushButton, QListWidgetItem
+from PyQt5.QtWidgets import QApplication, QFileDialog, QTextEdit, QLabel, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QListWidget, QCheckBox, QLineEdit, QPushButton, QListWidgetItem
 from PyQt5.QtGui import QFont
 
 class MyApp(QMainWindow):
@@ -15,6 +15,7 @@ class MyApp(QMainWindow):
 
         self.steps = data["steps"]
         self.sub_step_descriptions = data["sub_step_descriptions"]
+        self.load_steps_and_descriptions_from_yaml("steps.yaml")
 
         # Initialize stuff
         self.checkbox_states = {main_step: [False]*len(sub_steps) for main_step, sub_steps in self.steps.items()}
@@ -28,6 +29,8 @@ class MyApp(QMainWindow):
         self.pdu_var = ""
         self.bmc_var = ""
         self.server_var = ""
+        self.user_notes = {}
+        self.current_step_name = None
 
         self.initUI()
 
@@ -56,9 +59,12 @@ class MyApp(QMainWindow):
         self.rack_label = QLabel("Full Rack Name: ", self)
         self.mtm_label = QLabel("MTM: ", self)
         self.confirm_button = QPushButton("Confirm", self)
+        self.confirm_button.clicked.connect(self.confirm_inputs)
         self.save_button = QPushButton("Save", self)
+        self.save_button.clicked.connect(self.save_button_clicked)
         self.load_button = QPushButton("Load", self)
-
+        self.load_button.clicked.connect(self.load_button_clicked)
+        
         layout_top_h = QHBoxLayout()
         layout_top_h.addWidget(self.sys_label)
         layout_top_h.addWidget(self.sys_input)
@@ -67,32 +73,35 @@ class MyApp(QMainWindow):
         layout_top_h.addWidget(self.mtm_label)
         layout_top_h.addWidget(self.mtm_input)
         layout_top_h.addWidget(self.confirm_button)
-        self.confirm_button.clicked.connect(self.confirm_inputs)
         layout_top_h.addWidget(self.save_button)
         layout_top_h.addWidget(self.load_button)
-
-        layout_right_v.addLayout(layout_top_h)
 
         # Right pane for sub steps
         self.sub_steps_widget = QWidget()
         self.sub_steps_layout = QVBoxLayout()
         self.sub_steps_widget.setLayout(self.sub_steps_layout)
+        self.sub_step_detail_layout = QVBoxLayout()
 
-        # Add a QTextEdit widget to show sub-step details
+        # Pre-create the QTextEdit widgets and add them to sub_step_detail_layout
         self.sub_step_detail_text = QTextEdit()
-        self.sub_step_detail_text.setReadOnly(True)  # Make it read-only
+        self.sub_step_detail_text.setReadOnly(True)
+        self.sub_step_detail_layout.addWidget(self.sub_step_detail_text)
+        
+        self.user_input_text = QTextEdit()
+        self.user_input_text.setPlaceholderText("Your notes here...")
+        self.sub_step_detail_layout.addWidget(self.user_input_text)
+
+        layout_right_v.addLayout(layout_top_h)
         layout_right_v.addWidget(self.sub_steps_widget)
-        layout_right_v.addWidget(self.sub_step_detail_text)  # Add to the layout
-
-
-        layout_right_v.addWidget(self.sub_steps_widget)
-
+        layout_right_v.addLayout(self.sub_step_detail_layout)
+    
         layout_h.addLayout(layout_left_v, 1)
         layout_h.addLayout(layout_right_v, 3)
 
         central_widget = QWidget()
         central_widget.setLayout(layout_h)
         self.setCentralWidget(central_widget)
+
 
     def on_main_step_clicked(self, item):
         # Clear the current sub steps
@@ -114,17 +123,6 @@ class MyApp(QMainWindow):
             self.sub_steps_layout.addWidget(chk_box)
 
         self.sub_steps_layout.addStretch()
-
-    def display_sub_step_detail(self, step_name):
-        """Slot to handle sub-step checkbox clicks."""
-        # Fetch the predefined text
-        predef_text = self.sub_step_descriptions.get(step_name, "")
-        
-        # Replace placeholders with variable values
-        modified_text = predef_text.format(sys=self.sys_var, rack=self.rack_var, plain_rack=self.plain_rack_var, mtm=self.mtm_var, mtor=self.mtor_var, tor=self.tor_var, pdu=self.pdu_var, bmc=self.bmc_var, server=self.server_var)
-        
-        # Update the QTextEdit with the modified text
-        self.sub_step_detail_text.setPlainText(modified_text)
 
     def on_checkbox_state_changed(self, main_step, index, state):
         """Slot for checkbox state changed signal."""
@@ -163,6 +161,128 @@ class MyApp(QMainWindow):
         
         self.steps = data.get('steps', {})
         self.sub_step_descriptions = data.get('sub_step_descriptions', {})
+
+    def display_sub_step_detail(self, step_name):
+        # Fetch and format the predefined text
+        predef_text = self.sub_step_descriptions.get(step_name, "")
+        modified_text = predef_text.format(sys=self.sys_var, rack=self.rack_var, mtm=self.mtm_var, 
+                                        plain_rack=self.plain_rack_var, mtor=self.mtor_var, tor=self.tor_var, 
+                                        pdu=self.pdu_var, bmc=self.bmc_var, server=self.server_var)
+        
+        # Add predefined details
+        self.sub_step_detail_text.setPlainText(modified_text)
+
+        user_note_widget = self.user_notes.get(step_name)
+        if user_note_widget is not None:
+            user_note = user_note_widget.toPlainText()
+        else:
+            user_note = ""
+
+        self.user_input_text.setPlainText(user_note)
+        
+        # Update the current step name
+        self.current_step_name = step_name
+
+    def create_save_data(self):
+        save_data = {
+            'sys_var': self.sys_var,
+            'rack_var': self.rack_var,
+            'mtm_var': self.mtm_var,
+            'plain_rack_var' : self.plain_rack_var,
+            'mtor_var' : self.mtor_var,
+            'tor_var' : self.tor_var,
+            'pdu_var' : self.pdu_var,
+            'bmc_var' : self.bmc_var,
+            'server_var' : self.server_var,
+            'steps': self.steps,
+            'user_notes': {step_name: text_edit.toPlainText() for step_name, text_edit in self.user_notes.items()},
+            'checkbox_states': self.checkbox_states
+            # Add more data when there's new stuff
+        }
+        return save_data
+
+    def serialize_save_data(self, save_data):
+        return yaml.dump(save_data)
+
+    def save_to_file(self, yaml_str, filename):
+        with open(filename, 'w') as file:
+            file.write(yaml_str)
+
+    def save_button_clicked(self):
+        save_data = self.create_save_data()
+        yaml_str = self.serialize_save_data(save_data)
+        filename = f"{self.sys_var}-{self.rack_var}.yaml"
+        self.save_to_file(yaml_str, filename)
+
+    def load_button_clicked(self):
+        # You may use QFileDialog to get the filename to load
+        filename, _ = QFileDialog.getOpenFileName(self, "Load session", "", "YAML Files (*.yaml)")
+        if filename:
+            self.load_session_from_file(filename)
+
+    def load_session_from_file(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                data = yaml.safe_load(file)
+
+            # Apply loaded settings
+            self.sys_var = data.get('sys_var', "")
+            self.rack_var = data.get('rack_var', "")
+            self.mtm_var = data.get('mtm_var', "")
+            self.plain_rack_var = data.get('plain_rack_var', "")
+            self.mtor_var = data.get('mtor_var', "")
+            self.tor_var = data.get('tor_var', "")
+            self.pdu_var = data.get('pdu_var', "")
+            self.bmc_var = data.get('bmc_var', "")
+            self.server_var = data.get('server_var', "")
+            loaded_user_notes = data.get('user_notes', {})
+            self.user_notes = {}
+            self.checkbox_states = data.get('checkbox_states', {})
+
+            # Assume 'main_step_name' is correctly acquired from UI or data. Otherwise, modify as needed.
+            main_step_name = (
+                self.main_steps_view.currentItem().text() 
+                if self.main_steps_view.currentItem() 
+                else None
+            )
+            if main_step_name:
+                # Regenerate sub-steps UI to reflect loaded checkbox states
+                self.on_main_step_clicked(self.main_steps_view.currentItem())
+                
+            # Update UI Elements with Loaded Data
+            self.sys_input.setText(self.sys_var)
+            self.rack_input.setText(self.rack_var)
+            self.mtm_input.setText(self.mtm_var)
+
+            for step_name, note in loaded_user_notes.items():
+                if step_name in self.user_notes:
+                    # If there's already a QTextEdit for this step, update its content
+                    self.user_notes[step_name].setPlainText(note)
+                else:
+                    # Otherwise, create a new QTextEdit, set its content, and store it in the dictionary for future reference
+                    text_edit = QTextEdit(self)
+                    text_edit.setPlainText(note)
+                    self.user_notes[step_name] = text_edit
+                    
+                    # Ensure the new QTextEdit is properly added to your UI
+                    # E.g., self.sub_step_detail_layout.addWidget(text_edit)
+
+
+
+            current_item = self.main_steps_view.currentItem()
+            if current_item:
+                self.on_main_step_clicked(current_item)
+                    
+        except FileNotFoundError:
+            print(f"Error: File {filename} not found.")
+            # Optionally: Display a user-friendly error message in your GUI.
+        except yaml.YAMLError as ye:
+            print(f"Error parsing YAML file {filename}: {str(ye)}")
+            # Optionally: Display a user-friendly error message in your GUI.
+        except Exception as e:
+            print(f"Unexpected error loading session from {filename}: {str(e)}")
+            # Optionally: Display a user-friendly error message in your GUI.
+
 
 
 if __name__ == '__main__':
